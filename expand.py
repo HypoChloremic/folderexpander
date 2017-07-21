0c46d3e06807391a267c328fdacadeadcc5eb62c
HypoChloremic <ali.rassolie@gmail.com>
2017-07-21 14:48:29 +0200

    Documentation

--- expand.py
+++ expand.py
@@ -1,5 +1,12 @@
 import os
 
+
+# The file expander takes a parent direcotry and a number,
+# indicating the number of folders to be opened, before returning 
+# the contents of that folder. In this case, the default delimiter
+# is adapted to the Ubuntu environment, and not the Windows one,
+# where paths are delimited with a '\'. 
+
 def file_expander(parent, n=3, delimiter="/"):
 	for i in range(n):
 		if i == n-1:
@@ -11,7 +18,10 @@ def file_expander(parent, n=3, delimiter="/"):
 		else: 
 			parent = expand(parent, delimiter)
 			
-
+# The expand method allows permits a recursive function above, where
+# we get to parse a directory to be opened and then return the folders
+# of that directory, before moving on with the loop.
+# this is why it is important that the parameter directory is of type list. 
 def expand(f, delimiter="/"):
 	l = []
 	for each in f:
@@ -21,8 +31,7 @@ def expand(f, delimiter="/"):
 			l.append(f"{each}{delimiter}{i}")	
 	return l
 
+if __name__ == '__main__':
 	
-
-
-k = file_expander([os.path.abspath(r"C:\Users\Ali Rassolie\OneDrive\prwork\python\packages")], delimiter="\\")
-print(k)
+	k = file_expander([os.path.abspath(r"C:\Users\Ali Rassolie\OneDrive\prwork\python\packages")], delimiter="\\")
+	print('\n'.join(k))
